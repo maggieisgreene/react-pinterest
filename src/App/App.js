a6b14f9eb9a3297c1030c579ef300f28bf7a2f06
Maggie Greene <maggie.greene96@gmail.com>
2019-12-10 21:34:50 -0600

    Login button authenticates user

--- src/App/App.js
+++ src/App/App.js
@@ -1,13 +1,45 @@
 import React from 'react';
-import './App.scss';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import firebase from 'firebase/app';
+import 'firebase/auth';
 
-function App() {
-  return (
-    <div className="App">
-      <button className="btn btn-danger">Bootstrap Button</button>
-    </div>
-  );
+import firebaseConnection from '../helpers/data/connection';
+import Auth from '../components/Auth/Auth';
+
+import './App.scss';
+
+firebaseConnection();
+
+class App extends React.Component {
+  state = {
+    authed: false,
+  }
+
+  componentDidMount() {
+    this.removeListener = firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ authed: true });
+      } else {
+        this.setState({ authed: false });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.removeListener();
+  }
+
+  render() {
+    const { authed } = this.state;
+
+    return (
+      <div className="App">
+        <button className="btn btn-danger">Bootstrap Button</button>
+        {
+        (authed) ? (<div>You logged in!</div>) : (<Auth />)
+        }
+      </div>
+    );
+  }
 }
 
 export default App;
