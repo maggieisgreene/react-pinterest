178b9ce1449a07d60c35e02a3944c1c238c85da3
Maggie Greene <maggie.greene96@gmail.com>
2019-12-14 12:08:13 -0600

    Reading boards complete

--- src/components/BoardsContainer/BoardsContainer.js
+++ src/components/BoardsContainer/BoardsContainer.js
@@ -1,5 +1,7 @@
 import React from 'react';
 
+import Board from '../Board/Board';
+
 import authData from '../../helpers/data/authData';
 import boardData from '../../helpers/data/boardData';
 
@@ -18,7 +20,7 @@ class BoardsContainer extends React.Component {
 
   render() {
     return (
-      <div>{this.state.boards.map((board) => <h6>{board.name}</h6>)}</div>
+      <div>{this.state.boards.map((board) => <Board key={board.id} board={board} />)}</div>
     );
   }
 }
